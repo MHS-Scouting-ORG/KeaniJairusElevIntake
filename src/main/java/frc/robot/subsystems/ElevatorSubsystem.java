d30848f86a86099c4b997e96599dbab58c3291b7
KeaniKina <keani1028@gmail.com>
2024-01-15 10:54:48 -1000

    Elevator Methods added

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -8,9 +9,24 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ElevatorSubsystem extends SubsystemBase {
 
   private CANSparkMax elevMotor;
+  private RelativeEncoder enc;
 
   public ElevatorSubsystem() {
     elevMotor = new CANSparkMax(0, MotorType.kBrushless);
+    enc = elevMotor.getEncoder();
+  }
+
+  public void ManualElevator(double speed) {
+    elevMotor.set(deadzone(speed));
+  }
+
+  public double deadzone(double speed) {
+    if (Math.abs(speed) < 0.1) {
+      return 0;
+    }
+    else {
+      return speed;
+    }
   }
 
   @Override
