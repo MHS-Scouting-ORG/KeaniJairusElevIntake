8d50b4daf86fdcb0fe8f4421d53f2a624f459fd3
KeaniKina <keani1028@gmail.com>
2024-01-15 14:58:38 -1000

    removed a dot

--- src/main/java/frc/robot/commands/ElevatorCommands/ElevatorToBottomCommand.java
+++ src/main/java/frc/robot/commands/ElevatorCommands/ElevatorToBottomCommand.java
@@ -4,7 +4,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ElevatorSubsystem;
 
 public class ElevatorToBottomCommand extends Command {
-
   private ElevatorSubsystem elevSub;
 
   public ElevatorToBottomCommand(ElevatorSubsystem newElevSub) {
