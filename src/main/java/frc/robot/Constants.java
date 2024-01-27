d826986be2dc015310680d88cde187b1f2749442
KeaniKina <keani1028@gmail.com>
2024-01-27 12:07:42 -1000

    Added Intake constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@ public final class Constants {
   public static class IntakeConstants {
     public static final int INTAKE_PORT = 1;
     public static final int INTAKEPIVOT_PORT = 5;
+    public static final int INTAKE_RESTING_LS_PORT = 9;
+    public static final int INTAKE_INTAKING_LS_PORT = 8;
     public static final double INTAKEPIVOT_KP = 0.01;
     public static final double INTAKEPIVOT_KI = 0;
     public static final double INTAKEPIVOT_KD = 0;
