1b8f94a9b41641e8fb3a43506f41fc45432e9d2e
JairusTumacder <tumacderjairus7@gmail.com>
2024-02-03 14:17:23 -1000

    removed xbox controller code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,7 +1,6 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -20,7 +19,6 @@ public class RobotContainer {
   private final HangSubsystem hangSub = new HangSubsystem();
   private final UnderIntakeSubsystem u_subsystem = new UnderIntakeSubsystem();
 
-  //private final XboxController xboxController = new XboxController(0);
   private final Joystick joystick = new Joystick(1);
 
   public RobotContainer() {
