ac58fcc920b43473be3cc0e6344307b2bb176a77
KeaniKina <keani1028@gmail.com>
2024-02-20 16:59:59 -1000

    Uncommented code for testing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -34,14 +34,14 @@ public class RobotContainer {
   public RobotContainer() {
     //i_subsystem.setDefaultCommand(new ManualIntakePivot(u_subsystem, () -> joystick.getY()));
     //hangSub.setDefaultCommand(new ManualHangCmd(hangSub, () -> joystick.getY()));
-    elevSub.setDefaultCommand(new ManualElevatorCmd(elevSub, () -> joystick.getY()));
-    //u_subsystem.setDefaultCommand(new ManualIntakeCmd(u_subsystem, () -> joystick.getY()));
+    //elevSub.setDefaultCommand(new ManualElevatorCmd(elevSub, () -> joystick.getY()));
+    u_subsystem.setDefaultCommand(new ManualIntakeCmd(u_subsystem, () -> joystick.getY()));
     configureBindings();
   }
 
   private void configureBindings() {
     
-    new JoystickButton(joystick, 1).onTrue(new IntakeCmd(u_subsystem,() ->joystick.getY(), () -> joystick.getX()));
+    //new JoystickButton(joystick, 1).onTrue(new IntakeCmd(u_subsystem,() ->joystick.getY(), () -> joystick.getX()));
     //new JoystickButton(joystick, 3).toggleOnTrue(new OuttakeCmd(u_subsystem));
     //new JoystickButton(joystick, 3).toggleOnFalse(new InstantCommand(() -> u_subsystem.stopIntake()));
     //new JoystickButton(joystick, 2).onTrue(new DeliverCmd(u_subsystem));
