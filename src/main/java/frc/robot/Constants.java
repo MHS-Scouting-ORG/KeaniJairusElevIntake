03dca03fc855f530dc1bf4cfbfc3e3112ee1b5cc
KeaniKina <keani1028@gmail.com>
2024-03-02 11:08:28 -1000

    Upped elevator speedcap from 0.3 to 0.6

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -36,7 +36,7 @@ public final class Constants {
     public static final int TOP_LS_PORT = 3;
     public static final int BOTTOM_LS_PORT = 0;
 
-    public static final double SPEED_CAP = 0.3;
+    public static final double SPEED_CAP = 0.6;
 
     public static final double ELEV_KP = 0.03;
     public static final double ELEV_KI = 0.0001;
