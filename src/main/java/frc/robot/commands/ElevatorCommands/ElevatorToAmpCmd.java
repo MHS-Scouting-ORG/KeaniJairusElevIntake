31d154f4b2e2db5c7f0daf0775919149fa34a971
JairusTumacder <tumacderjairus7@gmail.com>
2024-03-08 18:27:01 -1000

    created a elevator cmd for the amp position

---
+++ src/main/java/frc/robot/commands/ElevatorCommands/ElevatorToAmpCmd.java
@@ -0,0 +1,35 @@
+package frc.robot.commands.ElevatorCommands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ElevatorSubsystem;
+
+public class ElevatorToAmpCmd extends Command {
+
+  private ElevatorSubsystem elevSub;
+
+  public ElevatorToAmpCmd(ElevatorSubsystem newElevSub) {
+    elevSub = newElevSub;
+  
+    addRequirements(elevSub);
+  }
+
+  @Override
+  public void initialize() {
+  }
+
+  @Override
+  public void execute() {
+    // Amp is 155
+    elevSub.setSetpoint(155);
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    elevSub.elevStop();
+  }  
+
+  @Override
+  public boolean isFinished() {
+    return elevSub.getTopLimitSwitch() || elevSub.isAtSetpoint();
+  }
+}
